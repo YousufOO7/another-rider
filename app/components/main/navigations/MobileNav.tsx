98c2d2d441cfebb210b1631d88c639950cd08b2c
Yousuf007 <yousufjobaer@gmail.com>
2026-09-14 14:24:39 +0600

    show logo in mb

--- app/components/main/navigations/MobileNav.tsx
+++ app/components/main/navigations/MobileNav.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/set-state-in-effect */
 "use client";
 
@@ -9,7 +10,10 @@ import {
   SheetTrigger,
   SheetClose,
 } from "@/components/ui/sheet";
-import { INavigationLinks, publicNavigationLinks } from "@/app/utils/constant/navigations/publicNavigationLinks";
+import {
+  INavigationLinks,
+  publicNavigationLinks,
+} from "@/app/utils/constant/navigations/publicNavigationLinks";
 import { usePathname, useRouter } from "next/navigation";
 import { Menu } from "lucide-react";
 import { appConfiguration } from "@/app/utils/constant/appConfiguration";
@@ -32,41 +36,63 @@ import { useAppConfig } from "@/app/utils/helper/useAppConfig";
 
 const MobileNav = () => {
   const pathname = usePathname();
-   const router = useRouter();
-    const [token, setToken] = useState<string | null>(null);
-  
-    useEffect(() => {
-      const t = shareWithCookies("get", `${appConfiguration.appCode}token`);
-      setToken(t || null);
-    }, []);
-  
-    const handleLogout = () => {
-      // Clear token from cookies
-      shareWithCookies("remove", `${appConfiguration.appCode}token`);
-      // Optionally clear user data from localStorage
-      localStorage.removeItem("user");
-      setToken(null);
-      router.push("/auth/login"); // redirect to login
-    };
-  
-      const linksToShow: INavigationLinks[] = publicNavigationLinks.filter((link) => {
+  const router = useRouter();
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const t = shareWithCookies("get", `${appConfiguration.appCode}token`);
+    setToken(t || null);
+  }, []);
+
+  const handleLogout = () => {
+    // Clear token from cookies
+    shareWithCookies("remove", `${appConfiguration.appCode}token`);
+    // Optionally clear user data from localStorage
+    localStorage.removeItem("user");
+    setToken(null);
+    router.push("/auth/login"); // redirect to login
+  };
+
+  const linksToShow: INavigationLinks[] = publicNavigationLinks.filter(
+    (link) => {
       if (token) {
         return true;
       } else {
         // Not logged in: hide "Customer Profile"
         return link.key !== "/profile/my-profile";
       }
-    });
+    },
+  );
 
-    const {platformName, isLoading} = useAppConfig();
+  const { platformLogo, isLoading } = useAppConfig();
 
   return (
     <nav className="border-b bg-white md:hidden">
       {/* Top Bar */}
       <div className="flex items-center justify-between px-4 py-3">
         {/* Logo */}
-        <Link href="/" className="text-lg font-bold">
+        {/* <Link href="/" className="text-lg font-bold">
           <span className="">{isLoading ? <ButtonLoader /> : platformName}</span>
+        </Link> */}
+        <Link href="/" className="text-xl font-bold text-gray-900">
+          {/* <span className="">{isLoading ? <ButtonLoader /> : platformName}</span> */}
+          <span className="flex items-center gap-2">
+            {isLoading ? (
+              <ButtonLoader />
+            ) : (
+              <>
+                {platformLogo && (
+                  <img
+                    src={platformLogo}
+                    alt="Platform Logo"
+                    className=" h-8 bg-cover rounded"
+                  />
+                )}
+
+                {/* <span>{platformName || "Add Name"}</span> */}
+              </>
+            )}
+          </span>
         </Link>
 
         {/* Menu Trigger */}
@@ -102,45 +128,49 @@ const MobileNav = () => {
               {/* Bottom Login */}
               <div className="px-6 pb-6">
                 <SheetClose asChild>
-                   {token ? (
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button
-                variant="default"
-                size="sm"
-                className="cursor-pointer flex items-center gap-2"
-              >
-                <FiLogOut /> Logout
-              </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Confirm Logout</AlertDialogTitle>
-                <AlertDialogDescription>
-                  Are you sure you want to logout? You will need to login again
-                  to access your account.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel className="btn-destructive-fill cursor-pointer">
-                  Cancel
-                </AlertDialogCancel>
-                <AlertDialogAction
-                  className="cursor-pointer"
-                  onClick={handleLogout}
-                >
-                  Confirm
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        ) : (
-          <Link href={"/auth/login"}>
-            <Button variant="default" size="sm" className="cursor-pointer">
-              Login
-            </Button>
-          </Link>
-        )}
+                  {token ? (
+                    <AlertDialog>
+                      <AlertDialogTrigger asChild>
+                        <Button
+                          variant="default"
+                          size="sm"
+                          className="cursor-pointer flex items-center gap-2"
+                        >
+                          <FiLogOut /> Logout
+                        </Button>
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>Confirm Logout</AlertDialogTitle>
+                          <AlertDialogDescription>
+                            Are you sure you want to logout? You will need to
+                            login again to access your account.
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel className="btn-destructive-fill cursor-pointer">
+                            Cancel
+                          </AlertDialogCancel>
+                          <AlertDialogAction
+                            className="cursor-pointer"
+                            onClick={handleLogout}
+                          >
+                            Confirm
+                          </AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
+                  ) : (
+                    <Link href={"/auth/login"}>
+                      <Button
+                        variant="default"
+                        size="sm"
+                        className="cursor-pointer"
+                      >
+                        Login
+                      </Button>
+                    </Link>
+                  )}
                 </SheetClose>
               </div>
             </div>
